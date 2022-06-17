db684f903581d10ab73728822b6a21ca6a7e6fda
BossNumber1 <dimaang23@gmail.com>
2022-06-17 18:15:16 +0300

    added the desired design to the command block

--- app/pages/components/main/Main.jsx
+++ app/pages/components/main/Main.jsx
@@ -9,8 +9,10 @@ function Main() {
         {
             index: 1,
             introduction: `1. В терминале заходим в пустую созданную для нашего проекта дерикторию. Там вводим:`,
-            command: `npx create-react-app .`,
+            command: ["npx create-react-app ."],
             commandNumber: 0,
+            heightContent: 18,
+            widthContent: 162,
         },
         {
             index: 2,
@@ -20,126 +22,129 @@ function Main() {
             index: 3,
             introduction: `3. Переходим в папку с приложением и создаём Dockerfile.prod со
                 следующей структурой:`,
-            command: ` FROM node as build
-                    <br />
-                    WORKDIR /app
-                    <br />
-                    COPY package.json .<br />
-                    RUN npm install
-                    <br />
-                    COPY . .<br />
-                    ARG REACT_APP_NAME
-                    <br />
-                    ENV REACT_APP_NAME=$REACT_APP_NAME
-                    <br />
-                    RUN npm run build
-                    <br />
-                    <br />
-                    FROM nginx
-                    <br />
-                    COPY --from=build /app/build /usr/share/nginx/html`,
+            // command: [
+            //     "FROM node as build",
+            //     "WORKDIR /app",
+            //     "COPY package.json .",
+            //     "RUN npm install",
+            //     "COPY . .",
+            //     "ARG REACT_APP_NAME",
+            //     "ENV REACT_APP_NAME=$REACT_APP_NAME",
+            //     "RUN npm run build",
+            //     "",
+            //     "FROM nginx",
+            //     "COPY --from=build /app/build /usr/share/nginx/html",
+            // ],
+            command: [
+                ` FROM node as build \n WORKDIR /app \n COPY package.json . \n RUN npm install \n COPY . . \n ARG REACT_APP_NAME \n 
+ ENV REACT_APP_NAME=$REACT_APP_NAME \n RUN npm run build \n \n FROM nginx \n COPY --from=build /app/build /usr/share/nginx/html`,
+            ],
             commandNumber: 1,
+            heightContent: 188,
+            widthContent: 383,
         },
         {
             index: 4,
             introduction: `4. Создаём рядом Dockerfile.dev со следующим содержимым:`,
-            command: `FROM node
-                    <br />
-                    WORKDIR /app
-                    <br />
-                    COPY package.json .<br />
-                    RUN npm install
-                    <br />
-                    COPY . .<br />
-                    ENV REACT_APP_NAME=myName
-                    <br />
-                    EXPOSE 3000
-                    <br />
-                    CMD ["npm", "start"]
-                    <br />`,
+            // command: [
+            //     "FROM node",
+            //     "WORKDIR /app",
+            //     "COPY package.json .",
+            //     "RUN npm install",
+            //     "COPY . .",
+            //     "ENV REACT_APP_NAME=myName",
+            //     "EXPOSE 3000",
+            //     `CMD ["npm", "start"]`,
+            // ],
+            command: [
+                ` FROM node \n WORKDIR /app \n COPY package.json . \n RUN npm install \n COPY . . \n ENV REACT_APP_NAME=myName \n EXPOSE 3000 \n CMD ["npm", "start"]`,
+            ],
             commandNumber: 2,
+            heightContent: 127,
+            widthContent: 204,
         },
         {
             index: 5,
             introduction: `5. Теперь по соседству делаем файл .env:`,
-            command: `CHOKIDAR_USEPOLLING=true
-                    <br />
-                    REACT_APP_NAME=greatdev`,
+            command: [` CHOKIDAR_USEPOLLING=true \n REACT_APP_NAME=greatdev`],
             commandNumber: 3,
+            heightContent: 33,
+            widthContent: 195,
         },
         {
             index: 6,
             introduction: `6. Аналогично - docker-compose-dev.yml:`,
-            command: `version: '3.8'
-                    <br />
-                    services:
-                    <br />
-                    react-app:
-                    <br />
-                    build:
-                    <br />
-                    context: .<br />
-                    dockerfile: Dockerfile.dev
-                    <br />
-                    ports:
-                    <br />
-                    - "3000:3000"
-                    <br />
-                    volumes:
-                    <br />
-                    - ./src:/app/src
-                    <br />
-                    environment:
-                    <br />
-                    - REACT_APP_NAME=greatdev-dev
-                    <br />- CHOKIDAR_USEPOLLING=true`,
+            // command: [
+            //     "version: '3.8'",
+            //     "services:",
+            //     "react-app:",
+            //     "build:",
+            //     "context: .",
+            //     "dockerfile: Dockerfile.dev",
+            //     "ports:",
+            //     `- "3000:3000"`,
+            //     "volumes:",
+            //     `- ./src:/app/src`,
+            //     "environment:",
+            //     "- REACT_APP_NAME=greatdev-dev",
+            //     "- CHOKIDAR_USEPOLLING=true",
+            // ],
+            command: [
+                ` version: '3.8' \n services: \n react-app: \n build: \n context: . \n dockerfile: Dockerfile.dev \n ports: \n - "3000:3000" \n volumes: \n - ./src:/app/src \n environment: \n - REACT_APP_NAME=greatdev-dev \n - CHOKIDAR_USEPOLLING=true`,
+            ],
             commandNumber: 4,
+            heightContent: 202,
+            widthContent: 229,
         },
         {
             index: 7,
             introduction: `7. Рядышком - docker-compose-prod.yml:`,
-            command: `version: '3.8'
-                    <br />
-                    services:
-                    <br />
-                    react-app:
-                    <br />
-                    build:
-                    <br />
-                    context: .<br />
-                    dockerfile: Dockerfile.prod
-                    <br />
-                    args:
-                    <br />
-                    - REACT_APP_NAME=greatdev-prod
-                    <br />
-                    ports:
-                    <br />- "8080:80"`,
+            // command: [
+            //     "version: '3.8'",
+            //     "services:",
+            //     "react-app:",
+            //     "build:",
+            //     "context: .",
+            //     "dockerfile: Dockerfile.prod",
+            //     "args:",
+            //     "- REACT_APP_NAME=greatdev-prod",
+            //     "ports:",
+            //     `- "8080:80"`,
+            // ],
+            command: [
+                ` version: '3.8' \n services: \n react-app: \n build: \n context: . \n dockerfile: Dockerfile.prod \n args: \n - REACT_APP_NAME=greatdev-prod \n ports: \n - "8080:80"`,
+            ],
             commandNumber: 5,
+            heightContent: 155,
+            widthContent: 235,
         },
         {
             index: 8,
             introduction: `8. Туда же - .dockerignore:`,
-            command: `node_modules
-                    <br />
-                    Dockerfile
-                    <br />
-                    .git
-                    <br />
-                    .gitignore
-                    <br />
-                    .dockerignore
-                    <br />
-                    .env`,
+            // command: [
+            //     "node_modules",
+            //     "Dockerfile",
+            //     ".git",
+            //     ".gitignore",
+            //     ".dockerignore",
+            //     ".env",
+            // ],
+            command: [
+                ` node_modules \n Dockerfile \n .git \n .gitignore \n .dockerignore \n .env`,
+            ],
             commandNumber: 6,
+            heightContent: 95,
+            widthContent: 115,
         },
         {
             index: 9,
             introduction: `9. Теперь, для того чтобы посмотреть как будет в
                 prodakshen-режиме и как в режиме разработки, пропишем в
                 компоненте App.js:`,
-            command: "<p>{`Hello, ${process.env.REACT_APP_NAME}!`}</p>",
+            command: ["<p>{`Hello, ${process.env.REACT_APP_NAME}!`}</p>"],
             commandNumber: 7,
+            heightContent: 17,
+            widthContent: 355,
         },
     ];
 
@@ -183,6 +188,8 @@ function Main() {
                         introduction={el.introduction}
                         command={el.command}
                         commandNumber={el.commandNumber}
+                        heightContent={el.heightContent}
+                        widthContent={el.widthContent}
                     />
                 );
             })}
--- app/pages/components/main/Paragraph.jsx
+++ app/pages/components/main/Paragraph.jsx
@@ -1,13 +1,52 @@
-function Paragraph({ introduction, command, commandNumber }) {
+import React from "react";
+
+function Paragraph({
+    introduction,
+    command,
+    commandNumber,
+    heightContent,
+    widthContent,
+}) {
+    const [commands, setCommands] = React.useState(false);
+
+    React.useEffect(() => {
+        if (command && !commands) {
+            let commandList = command.map((el) => {
+                return el + "\n";
+            });
+
+            setCommands(commandList);
+        } else {
+            console.log("commands =", commands);
+        }
+    }, [commands]);
+
+    // const copyCommand = (numberCommand) => {
+    //     const copiedTextInput =
+    //         document.getElementsByClassName("monospace")[+numberCommand]
+    //             .children[0];
+
+    //     copiedTextInput.select();
+    //     document.execCommand("copy");
+    // };
+
     return (
         <div className={command ? "paragraph" : "paragraph paragraph_noCode"}>
             {introduction}
             {command && (
                 <div className="monospace">
-                    <input value={command} readonly />
-                    <button onclick={`copyCommand('${commandNumber}')`}>
+                    {/* <textarea value={commands} style={{ height: 120 + "px" }} /> */}
+                    {/* <input dangerouslySetInnerHTML={{ __html: commands }} /> */}
+
+                    <textarea
+                        value={command}
+                        style={{ height: heightContent, width: widthContent }}
+                        readOnly
+                    />
+
+                    {/* <button onClick={copyCommand(commandNumber)}>
                         <img src="https://clck.ru/r5L6U" />
-                    </button>
+                    </button> */}
                 </div>
             )}
         </div>
