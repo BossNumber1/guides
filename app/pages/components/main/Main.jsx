4a8e135cb38a27662a21c8770989ca41f4a9814e
BossNumber1 <dimaang23@gmail.com>
2022-06-20 14:57:24 +0300

    update the commands

--- app/pages/components/main/Main.jsx
+++ app/pages/components/main/Main.jsx
@@ -50,19 +50,19 @@ function Main() {
             index: 6,
             introduction: `6. Аналогично - docker-compose-dev.yml:`,
             command: [
-                ` version: '3.8' \n services: \n   react_example: \n     build: \n       context: . \n       dockerfile: Dockerfile.dev \n     container_name: container_react_example \n     ports: \n       - "3000:3000" \n     volumes: \n       - ./src:/app/src \n     environment: \n       - REACT_APP_NAME=greatdev-dev \n       - CHOKIDAR_USEPOLLING=true`,
+                ` version: '3.8' \n services: \n   react_example: \n     build: \n       context: . \n       dockerfile: Dockerfile.dev \n     container_name: dev_container_react_example \n     ports: \n       - "3000:3000" \n     volumes: \n       - ./src:/app/src \n     environment: \n       - REACT_APP_NAME=greatdev-dev \n       - CHOKIDAR_USEPOLLING=true`,
             ],
             heightContent: 219,
-            widthContent: 336,
+            widthContent: 363,
         },
         {
             index: 7,
             introduction: `7. Рядышком - docker-compose-prod.yml:`,
             command: [
-                ` version: '3.8' \n services: \n   react_example: \n     build: \n       context: . \n       dockerfile: Dockerfile.prod \n     args: \n       - REACT_APP_NAME=greatdev-prod \n     ports: \n       - "8080:80"`,
+                ` version: '3.8' \n services: \n   react_example: \n     build: \n       context: . \n       dockerfile: Dockerfile.prod \n     container_name: prod_container_react_example \n     args: \n       - REACT_APP_NAME=greatdev-prod \n     ports: \n       - "8080:80"`,
             ],
-            heightContent: 155,
-            widthContent: 290,
+            heightContent: 171,
+            widthContent: 365,
         },
         {
             index: 8,
