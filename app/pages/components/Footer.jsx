a3afadcf6ec0a691d7da496439a2f20089c40dbe
BossNumber1 <dimaang23@gmail.com>
2022-06-22 14:43:11 +0300

    added icons and hints

--- app/pages/components/Footer.jsx
+++ app/pages/components/Footer.jsx
@@ -4,6 +4,15 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer__signature">Устинов Дмитрий, 2022</div>
+
+            <div className="footer__icons">
+                <div data-tooltip="Докеризация ReactJS">
+                    <img src="https://clck.ru/rcVkw" alt="reactjs icon" />
+                </div>
+                <div data-tooltip="Докеризация NextJS">
+                    <img src="https://clck.ru/rcWhq" alt="nextjs icon" />
+                </div>
+            </div>
         </div>
     );
 }
