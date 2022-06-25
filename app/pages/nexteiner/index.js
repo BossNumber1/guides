a5142a59b5f3cb0f55f8ce639c186e86b589ca17
BossNumber1 <dimaang23@gmail.com>
2022-06-25 10:19:25 +0300

    Created two pages

---
+++ app/pages/nexteiner/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Nexteiner() {
+    return <h1>Докеризация Next</h1>;
+}
+
+export default Nexteiner;
---
+++ app/pages/reacteiner/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Reacteiner() {
+    return <h1>Докеризация React</h1>;
+}
+
+export default Reacteiner;
