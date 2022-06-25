a590ca5ac8bb8e7a4fecfcae6328181dd857fe88
BossNumber1 <dimaang23@gmail.com>
2022-06-25 12:55:19 +0300

    changed home page

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,18 +1,33 @@
-import Footer from "./components/footer/Footer";
-import Header from "./components/Header";
-import Main from "./components/main/Main";
+// import Footer from "./components/footer/Footer";
+// import Header from "./components/Header";
+// import Main from "./components/main/Main";
 import DeclarationHeader from "./DeclarationHeader";
+import styles from "../styles/home/home.module.scss";
 
 export default function Home() {
     return (
         <>
-            <DeclarationHeader />
+            <DeclarationHeader
+                title="Guides"
+                keywords="Guides, ReactJS, NextJS, docker, application"
+            />
 
-            <div className="container">
+            <div className={styles.container}>
+                <div className={styles.container__row}>
+                    <div className={styles.container__card}>1</div>
+                    <div className={styles.container__card}>2</div>
+                </div>
+                <div className={styles.container__row}>
+                    <div className={styles.container__card}>3</div>
+                    <div className={styles.container__card}>4</div>
+                </div>
+            </div>
+
+            {/* <div className="container">
                 <Header />
                 <Main />
                 <Footer />
-            </div>
+            </div> */}
         </>
     );
 }
