ca5c6dfc01462b0b959d01ee3166c35ab6e0141d
BossNumber1 <dimaang23@gmail.com>
2022-06-25 16:07:32 +0300

    refactored + made automatic link generation

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -3,6 +3,40 @@ import styles from "../styles/home/home.module.scss";
 import Link from "next/link";
 
 export default function Home() {
+    const listLinks = [
+        [
+            {
+                index: 1,
+                pathToPage: "/reacteiner",
+                iconLink: "./pictures/iconReactJS.svg",
+                alt: "reactjs icon",
+                tooltip: "Докеризация ReactJS",
+            },
+            {
+                index: 2,
+                pathToPage: "/nexteiner",
+                iconLink: "./pictures/iconNextJS.svg",
+                alt: "nextjs icon",
+                tooltip: "Докеризация NextJS",
+            },
+        ],
+        [
+            {
+                index: 3,
+                pathToPage: "/tgboteiner",
+                iconLink: "./pictures/iconTgBot.svg",
+                alt: "telegram icon",
+                tooltip: "Докеризация Telegram-бота",
+            },
+            {
+                index: 4,
+                pathToPage: "",
+                iconLink: "",
+                alt: "",
+            },
+        ],
+    ];
+
     return (
         <>
             <DeclarationHeader
@@ -11,35 +45,32 @@ export default function Home() {
             />
 
             <div className={styles.container}>
-                <div className={styles.container__row}>
-                    <div className={styles.container__card}>
-                        <Link href="/reacteiner">
-                            <img
-                                src="./pictures/iconReactJS.svg"
-                                alt="reactjs icon"
-                            />
-                        </Link>
-                    </div>
-                    <div className={styles.container__card}>
-                        <Link href="/nexteiner">
-                            <img
-                                src="./pictures/iconNextJS.svg"
-                                alt="nextjs icon"
-                            />
-                        </Link>
-                    </div>
-                </div>
-                <div className={styles.container__row}>
-                    <div className={styles.container__card}>
-                        <Link href="/tgboteiner">
-                            <img
-                                src="./pictures/iconTgBot.svg"
-                                alt="telegram icon"
-                            />
-                        </Link>
-                    </div>
-                    <div className={styles.container__card}>4</div>
-                </div>
+                {listLinks.map((element) => {
+                    return (
+                        <div className={styles.container__row}>
+                            {element.map((el) => {
+                                return (
+                                    <div
+                                        key={el.index}
+                                        className={
+                                            el.pathToPage !== ""
+                                                ? styles.container__card
+                                                : styles.container__defaultCard
+                                        }
+                                        data-tooltip={el.tooltip && el.tooltip}
+                                    >
+                                        <Link href={el.pathToPage}>
+                                            <img
+                                                src={el.iconLink}
+                                                alt={el.alt}
+                                            />
+                                        </Link>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
             </div>
         </>
     );
