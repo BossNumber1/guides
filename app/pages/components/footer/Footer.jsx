99ee36cc5d28e95002c34c9cb95be31a9ebd8d4d
BossNumber1 <dimaang23@gmail.com>
2022-06-26 09:05:04 +0300

    Did icons footer links

--- app/pages/components/footer/Footer.jsx
+++ app/pages/components/footer/Footer.jsx
@@ -1,12 +1,65 @@
-import React from "react";
+import Link from "next/link";
 
 function Footer({ numberIconActive }) {
+    const listLinks = [
+        [
+            {
+                index: 1,
+                pathToPage: "/reacteiner",
+                iconLink: "./pictures/iconReactJS.svg",
+                alt: "reactjs icon",
+                tooltip: "Докеризация ReactJS",
+            },
+            {
+                index: 2,
+                pathToPage: "/nexteiner",
+                iconLink: "./pictures/iconNextJS.svg",
+                alt: "nextjs icon",
+                tooltip: "Докеризация NextJS",
+            },
+        ],
+        [
+            {
+                index: 3,
+                pathToPage: "/tgboteiner",
+                iconLink: "./pictures/iconTgBot.svg",
+                alt: "telegram icon",
+                tooltip: "Докеризация Telegram-бота",
+            },
+            {
+                index: 4,
+                pathToPage: "",
+                iconLink: "",
+                alt: "",
+            },
+        ],
+    ];
+
     return (
         <div className="footer">
             <div className="footer__signature">Устинов Дмитрий, 2022</div>
 
             <div className="footer__icons">
-                <div
+                {listLinks.map((element) => {
+                    return element.map((el) => {
+                        return (
+                            <div
+                                data-tooltip={el.tooltip}
+                                className={
+                                    +numberIconActive === el.index - 1
+                                        ? "footer__icon footer__icon_active"
+                                        : "footer__icon"
+                                }
+                            >
+                                <Link href={el.pathToPage}>
+                                    <img src={el.iconLink} alt={el.alt} />
+                                </Link>
+                            </div>
+                        );
+                    });
+                })}
+
+                {/* <div
                     data-tooltip="Докеризация ReactJS"
                     className={
                         +numberIconActive === 0
@@ -14,7 +67,12 @@ function Footer({ numberIconActive }) {
                             : "footer__icon"
                     }
                 >
-                    <img src="./pictures/iconReactJS.svg" alt="reactjs icon" />
+                    <Link href={el.pathToPage}>
+                        <img
+                            src="./pictures/iconReactJS.svg"
+                            alt="reactjs icon"
+                        />
+                    </Link>
                 </div>
                 <div
                     data-tooltip="Докеризация NextJS"
@@ -24,7 +82,12 @@ function Footer({ numberIconActive }) {
                             : "footer__icon"
                     }
                 >
-                    <img src="./pictures/iconNextJS.svg" alt="nextjs icon" />
+                    <Link href={el.pathToPage}>
+                        <img
+                            src="./pictures/iconNextJS.svg"
+                            alt="nextjs icon"
+                        />
+                    </Link>
                 </div>
                 <div
                     data-tooltip="Докеризация Telegram-бота"
@@ -34,8 +97,13 @@ function Footer({ numberIconActive }) {
                             : "footer__icon"
                     }
                 >
-                    <img src="./pictures/iconTgBot.svg" alt="telegram icon" />
-                </div>
+                    <Link href={el.pathToPage}>
+                        <img
+                            src="./pictures/iconTgBot.svg"
+                            alt="telegram icon"
+                        />
+                    </Link>
+                </div> */}
             </div>
         </div>
     );
