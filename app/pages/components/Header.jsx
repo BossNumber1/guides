5f9286c333a3ad6d6e6a7ce6cf9067d95e23ac6c
BossNumber1 <dimaang23@gmail.com>
2022-06-26 17:19:04 +0300

    Animate the headers

--- app/pages/components/Header.jsx
+++ app/pages/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Typed from "react-typed";
 
 function Header({ title, subtitle }) {
     return (
         <div className="header">
             <div className="header__content">
-                <div className="header__title">{title}</div>
+                <div className="header__title">
+                    <Typed strings={[`${title}`]} typeSpeed={50} />
+                </div>
                 <div
                     className="header__benefit"
                     dangerouslySetInnerHTML={{ __html: subtitle }}
